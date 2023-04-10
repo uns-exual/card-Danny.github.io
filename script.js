3518ea794dceb5c148ad2c8f1e14a1779f5783d1
uns-exual <113960729+uns-exual@users.noreply.github.com>
2023-04-10 19:47:23 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach((card) => {
-    // Add a click event listener to each card.
-    card.addEventListener('click', () => {
-        // Toggle the "open" class on the card.
-        card.classList.toggle('open');
-    });
-});
+function myFunction() {
+    location.href = "skaters.html"
+}
